fa8f1108db2d2d1afc65d89849df0d11e25d6658
rjstone <778287+rjstone@users.noreply.github.com>
2025-06-24 16:33:15 -0700

    new: Replace site with Astro and template

---
+++ src/pages/index.md
@@ -0,0 +1,15 @@
+---
+layout: '@/layouts/LandingLayout.astro'
+title: RJSTONE.net
+background: true
+logo: false
+---
+
+This is\
+the minimal landing page\
+of the web site\
+of rjstone.\
+I have a [blog](/posts/)\
+and some [projects](/projects/).\
+There is also\
+an [about](/about/) section.
---
+++ src/theme.config.ts
@@ -0,0 +1,54 @@
+import { defineThemeConfig } from './types'
+
+export default defineThemeConfig({
+  site: 'https://rjstone.new',
+  title: 'RJSTONE.net',
+  description: 'Personal Website of RJStone',
+  author: 'Robert Stone',
+  navbarItems: [
+    { label: 'Blog', href: '/posts/' },
+    { label: 'Projects', href: '/projects/' },
+    { label: 'Tags', href: '/tags/' },
+    { label: 'About', href: '/about/' },
+    {
+      label: 'Other pages',
+      children: [
+        { label: 'Landing page', href: '/' },
+        { label: '404 page', href: '/404' },
+        { label: 'Tag: documentation', href: '/tags/documentation/' }
+      ]
+    }
+  ],
+  footerItems: [
+    {
+      icon: 'tabler--brand-github',
+      href: 'https://github.com/rjstone',
+      label: 'Github'
+    },
+    {
+      icon: 'tabler--rss',
+      href: '/feed.xml',
+      label: 'RSS feed'
+    }
+  ],
+
+  // optional settings
+  locale: 'en',
+  mode: 'dark',
+  modeToggle: true,
+  colorScheme: 'scheme-mono',
+  openGraphImage: undefined,
+  postsPerPage: 4,
+  projectsPerPage: 3,
+  scrollProgress: false,
+  scrollToTop: true,
+  tagIcons: {
+    tailwindcss: 'tabler--brand-tailwind',
+    astro: 'tabler--brand-astro',
+    documentation: 'tabler--book'
+  },
+  shikiThemes: {
+    light: 'vitesse-light',
+    dark: 'vitesse-black'
+  }
+})
